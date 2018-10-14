156cd94db668be98a7c54ce46fffe2a4500fe457
seluce <36071735+seluce@users.noreply.github.com>
2018-10-14 02:17:27 +0200

    .
    .

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-# Use this to add x510 to CM's lunch command menu
+# Use this to add NOTE_S to CM's lunch command menu
 for var in eng user userdebug; do
   add_lunch_combo lineage_NOTE_S-$var
 done
