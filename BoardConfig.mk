6410638d67784b61eaf669b3410d908b1b3b80de
seluce <36071735+seluce@users.noreply.github.com>
2018-10-24 00:56:22 +0200

    .
    .

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,9 @@ BOARD_SEPOLICY_DIRS := \
 # Use old sepolicy version
 POLICYVERS := 29
 
+# Seccomp filter
+BOARD_SECCOMP_POLICY += device/CUBOT/NOTE_S/seccomp
+
 BLOCK_BASED_OTA := false
 TARGET_LDPRELOAD += libxlog.so:libmtk_symbols.so
 
