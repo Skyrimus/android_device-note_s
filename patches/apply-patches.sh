adb8f36628cd8468fac21c069c74b44227b6eeb6
seluce <ferris.daehn@outlook.de>
2018-11-06 14:45:08 +0100

    change order
    change order

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd ../../../..
 cd system/core
+patch -p1 < ../../device/CUBOT/NOTE_S/patches/LogcatFix/0012-Fix-Liblog-printif.patch
+patch -p1 < ../../device/CUBOT/NOTE_S/patches/LogcatFix/0013-Prevent-spamming-audio.patch
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
-patch -p1 < ../../device/CUBOT/NOTE_S/patches/LogcatFix/0012-Fix-Liblog-printif.patch
-patch -p1 < ../../device/CUBOT/NOTE_S/patches/LogcatFix/0013-Prevent-spamming-audio.patch
 cd ../..
 cd bionic
 patch -p1 < ../device/CUBOT/NOTE_S/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
