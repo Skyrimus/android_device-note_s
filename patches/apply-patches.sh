035195338a6a9c71f6331c42fd501588051ca5dd
seluce <ferris.daehn@outlook.de>
2018-11-08 15:22:13 +0100

    test hwui patch
    test new hwui cache patch

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -23,7 +23,8 @@ cd ../..
 cd frameworks/base
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_base/0001-Fix-Minior-Bugs.patch
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_base/0002-frameworks_base.patch
-patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_base/0003-fix-out-of-memory-gl-crashes.patch
+# just try "0004-fixing of hwui crashing"-patch as alternate patch
+# patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_base/0003-fix-out-of-memory-gl-crashes.patch
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_base/0004-fixing-of-hwui-crashing.patch
 cd ../..
 cd system/netd
