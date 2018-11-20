3d118a32138772468a9e4c832b256fe42c54edcc
seluce <36071735+seluce@users.noreply.github.com>
2018-11-20 10:45:00 +0100

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -4,15 +4,17 @@ cd packages/apps/Settings
 patch -p1 < ../../../device/CUBOT/NOTE_S/patches/packages_apps_settings/0001-Lunch-MiraVision-from-Settings.patch
 patch -p1 < ../../../device/CUBOT/NOTE_S/patches/packages_apps_settings/0002-android_settings_developer_info.patch
 git clean -f -d
+cd ../../../
 cd system/core
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/system_core/0001-libnetutils-add-MTK-bits.patch
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/system_core/0002-init-double-the-number-of-available-environment-variables.patch
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/system_core/0003-healthd-correctly-report-battery-voltage-on-MTK-kernels.patch
-cd ..
-patch -p1 < ../device/CUBOT/NOTE_S/patches/0001-wifi-tethering-fix.patch
+cd ../..
+cd system
+patch -p1 < ../device/CUBOT/NOTE_S/patches/system_netd/0001-wifi-tethering-fix.patch
 cd ..
 cd frameworks/av
-patch -p1 < ../../device/CUBOT/NOTE_S/patches/0001-frameworks_av.patch
+patch -p1 < ../../device/CUBOT/NOTE_S/patches/frameworks_av/0001-frameworks_av.patch
 cd ../..
 cd external/wpa_supplicant_8
 patch -p1 < ../../device/CUBOT/NOTE_S/patches/external_wpa_supplicant_8/0001-Ignore-fake-nvram-wifi.patch
